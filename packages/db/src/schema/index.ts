de5109af072886c1ad7ea519b4d7d1b3e841fef2
Badryansah Bangsawan <Badryansah99@gmail.com>
2026-01-26 10:45:17 +0800

    optimze this code and finalisasi page form

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,1 +1,13 @@
-export {};
+import { pgTable, serial, text, date, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  nisn: text("nisn").notNull().unique(),
+  nama: text("nama").notNull(),
+  tanggalLahir: date("tanggal_lahir").notNull(),
+  telpon: text("telpon").notNull(),
+  email: text("email").notNull(),
+  username: text("username").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
